80670dc09562d7732170e5ad24599fe82e4237d9
kaqueDM1 <kaiquedemetriobrz@gmail.com>
2024-08-23 11:42:19 -0300

    fiz o exercicio 02

--- exercicio02/src/main/java/org/example/Main.java
+++ exercicio02/src/main/java/org/example/Main.java
@@ -1,28 +1,76 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Personagem jogador = new Personagem();
-        jogador.nome = "kaique";
-        jogador.vida = 10;
+        Personagem orc = new Personagem();
+        orc.nome = "Orc";
+        orc.vida = 100;
+        orc.ataque = 10;
 
-        Personagem inimigo = new Personagem();
-        inimigo.nome = "Lula";
-        inimigo.vida = 20;
+        Personagem goblin = new Personagem();
+        goblin.nome = "Goblin";
+        goblin.vida = 50;
+        goblin.ataque = 5;
 
-        Personagem piloto = new Kamikaze();
-        piloto.nome = "Piloto Japones";
-        piloto.vida = 10;
+        Personagem lobo = new Personagem();
+        lobo.nome = "Lobo";
+        lobo.vida = 75;
+        lobo.ataque = 15;
 
-        jogador.atacar(inimigo);
-        piloto.atacar(inimigo);
+        Personagem cavaleiro = new Personagem();
+        cavaleiro.nome = "Cavaleiro";
+        cavaleiro.vida = 200;
+        cavaleiro.ataque = 20;
 
-        System.out.println("Jogador: " + jogador.vida);
-        System.out.println("Lula: " + inimigo.vida);
-        System.out.println("Piloto: " + piloto.vida);
+        Personagem dragao = new Personagem();
+        dragao.nome = "Dragão";
+        dragao.vida = 500;
+        dragao.ataque = 50;
 
+        Personagem steve = new Personagem();
+        steve.nome = "Steve";
+        steve.vida = 100;
+        steve.ataque = 20;
+
+        List<Personagem> inimigos = new ArrayList<>();
+        inimigos.add(orc);
+        inimigos.add(goblin);
+        inimigos.add(lobo);
+        inimigos.add(cavaleiro);
+        inimigos.add(dragao);
+
+        Random random = new Random();
+
+        while (steve.personagemVivo() && inimigos.stream().anyMatch(Personagem::personagemVivo)){
+
+            Personagem inimigo;
+            do{
+                inimigo = inimigos.get(random.nextInt(inimigos.size()));
+            } while (!inimigo.personagemVivo());
+
+            System.out.println("Steve ataca " + inimigo.nome);
+            steve.atacar(inimigo);
+
+            for (Personagem adversario : inimigos){
+                if (adversario.personagemVivo() && steve.personagemVivo()){
+                    System.out.println(adversario.nome + " ataca Steve");
+                    adversario.atacar(steve);
+                }
+            }
+
+        }
+
+        if (!steve.personagemVivo()){
+            System.out.println("Steve foi derrotado!");
+        } else{
+            System.out.println("Todos os inimigos foram derrotados! Steve venceu!");
+        }
 
     }
 
--- exercicio02/src/main/java/org/example/Personagem.java
+++ exercicio02/src/main/java/org/example/Personagem.java
@@ -3,9 +3,29 @@ package org.example;
 public class Personagem {
     public String nome;
     public Integer vida;
+    public Integer ataque;
 
+
+    public  void receberDano(int dano){
+        vida -= dano;
+        if (vida <= 0) {
+            vida = 0;
+            System.out.println(nome + " foi derrotado!");
+        }
+
+    }
     public void atacar(Personagem alvo) {
-            System.out.println(alvo.nome + " Foi atacado por " + this.nome);
-            alvo.vida -= 5;
+           if (alvo.vida > 0){
+               alvo.receberDano(ataque);
+               if (alvo.vida == 0){
+                   System.out.println("Ataque de " + nome + " contra " + alvo.nome + " foi bem-sucedido!");
+               }
+           } else{
+               System.out.println("Ataque de " + nome + " falhou, pois " + alvo.nome + " já está derrotado.");
+           }
+    }
+
+    public boolean personagemVivo(){
+        return vida > 0;
     }
 }
