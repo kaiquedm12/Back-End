449c956091922cbd3d2260792661971fad727d27
kaqueDM1 <kaiquedemetriobrz@gmail.com>
2024-09-19 16:03:45 -0300

    finalizei o exercicio07

---
+++ exercicio07/src/main/java/com/example/exercicio07/controller/RecomendacaoFilmeController.java
@@ -0,0 +1,24 @@
+package com.example.exercicio07.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecomendacaoFilmeController {
+
+    @GetMapping("/recomendar")
+    public String recomendarFilme(@RequestParam String genero, @RequestParam String ambiente) {
+        if (genero.equalsIgnoreCase("acao") && ambiente.equalsIgnoreCase("futurista")) {
+            return "Homens de Preto";
+        } else if (genero.equalsIgnoreCase("drama") && ambiente.equalsIgnoreCase("futurista")) {
+            return "Arrival";
+        } else if (genero.equalsIgnoreCase("comedia") && ambiente.equalsIgnoreCase("medieval")) {
+            return "Shrek";
+        } else if (genero.equalsIgnoreCase("drama") && ambiente.equalsIgnoreCase("medieval")) {
+            return "Gladiador";
+        } else {
+            return "Nenhuma recomendação encontrada para esses parâmetros.";
+        }
+    }
+}
