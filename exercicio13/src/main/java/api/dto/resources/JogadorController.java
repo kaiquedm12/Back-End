d96a046aee754be4d4132dd161910bbb9d4cdf0f
kaiquedm12 <160547555+kaiquedm12@users.noreply.github.com>
2024-11-07 14:58:36 -0300

    exercicio13

---
+++ exercicio13/src/main/java/api/dto/resources/JogadorController.java
@@ -0,0 +1,31 @@
+package api.dto.resources;
+
+import api.dto.JogadorDTO;
+import service.JogadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/jogadores")
+public class JogadorController {
+
+    @Autowired
+    private JogadorService jogadorService;
+
+    @PostMapping
+    public ResponseEntity<JogadorDTO> criarJogador(@Validated @RequestBody JogadorDTO jogadorDTO) {
+        JogadorDTO jogadorCriado = jogadorService.criarJogador(jogadorDTO);
+        return ResponseEntity.ok(jogadorCriado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<JogadorDTO>> listarJogadores() {
+        List<JogadorDTO> jogadores = jogadorService.listarJogadores();
+        return ResponseEntity.ok(jogadores);
+    }
+
+}
