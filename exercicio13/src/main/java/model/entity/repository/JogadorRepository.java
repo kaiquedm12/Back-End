107b8edcb8f307e6aeddc4fe56783fd42e5dc460
kaiquedm12 <160547555+kaiquedm12@users.noreply.github.com>
2024-11-14 13:21:51 -0300

    Delete exercicio13 directory

--- exercicio13/src/main/java/model/entity/repository/JogadorRepository.java
+++
@@ -1,6 +0,0 @@
-package model.entity.repository;
-
-import model.entity.Jogador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Long> {}
